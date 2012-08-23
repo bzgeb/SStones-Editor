5ceaee3f6fdfe907ee1ed00970b1681227f0a2a1
Bronson Zgeb <bronson.zgeb@gmail.com>
2012-08-23 15:38:59 -0400

    Added Osc to dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject skipping-stones-editor "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.4.0"]
-                           [noir "1.3.0-beta3"]]
+                           [noir "1.3.0-beta3"]
+                           [overtone/osc-clj "0.7.1-bzgeb"]]
             :main skipping-stones-editor.server)
