c7bca773500412f94f268d94b1160450a6a7807a
Bronson Zgeb <bronson.zgeb@gmail.com>
2012-08-23 16:56:38 -0400

    Updated dependencies

--- project.clj
+++ project.clj
@@ -2,5 +2,17 @@
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3"]
+                           [jayq "0.1.0-SNAPSHOT"]
+                           [fetch "0.1.0-alpha2"]
                            [overtone/osc-clj "0.7.1-bzgeb"]]
+            :plugins [[lein-cljsbuild "0.2.4"]]
+            :cljsbuild
+            {:builds
+              [{:builds nil,
+                   :source-path "src",
+                   :compiler
+                   {:pretty-print true,
+                        :output-dir "resources/public/cljs",
+                        :output-to "resources/public/cljs/bootstrap.js",
+                        :optimizations :simple}}]}
             :main skipping-stones-editor.server)
