ea60aa582a49ff4ba350f4f56dc45099e000f087
Bronson Zgeb <bronson.zgeb@gmail.com>
2012-08-23 16:57:50 -0400

    Added remotes to send notes to pure data

--- src/skipping_stones_editor/controllers.clj
+++ src/skipping_stones_editor/controllers.clj
@@ -1,13 +1,20 @@
-(ns skipping-stones-editor.controllers
-  (:use overtone.osc))
+(ns skipping-stones-editor.controllers)
+(use 'overtone.osc)
+(use 'noir.fetch.remotes)
 
 (def PORT 9001)
 
 (def client (osc-client "localhost" PORT))
 
-(defn send-message []
+(defn send-note 
+  ([]
   (osc-send client "/test/voice" (int 0)))
+  ([note]
+  (osc-send client "/text/voice" (int note))))
+
+(defremote note [n]
+  (send-note n))
 
 (defn editor [layout]
-  (send-message)
-  (layout [:p "Welcome to skpping-stones-editor"]))
+  (send-note)
+  (layout [:p "Welcome to skipping-stones-editor"]))
