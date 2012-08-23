17b72aba6e4dc046aebd8b52438e041c0849b022
Bronson Zgeb <bronson.zgeb@gmail.com>
2012-08-23 16:58:14 -0400

    Added necessary js files

--- src/skipping_stones_editor/views/common.clj
+++ src/skipping_stones_editor/views/common.clj
@@ -1,12 +1,14 @@
 (ns skipping-stones-editor.views.common
   (:use [noir.core :only [defpartial]]
-        [hiccup.page :only [include-css html5]]))
+        [hiccup.page :only [include-js include-css html5]]))
 
 (defpartial layout [& content]
             (html5
               [:head
                [:title "skipping-stones-editor"]
-               (include-css "/css/reset.css")]
+               (include-css "/css/reset.css")
+               (include-js "https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js")]
               [:body
                [:div#wrapper
-                content]]))
+                content]
+               (include-js "/cljs/bootstrap.js")]))
